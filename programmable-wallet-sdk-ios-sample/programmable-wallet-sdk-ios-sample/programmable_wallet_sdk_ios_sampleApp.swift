35365777d883d901401e3876d5b27212b04b2817
Kevin Chiang <101379279+kevin-ios-circle@users.noreply.github.com>
2023-05-18 16:10:03 +0800

    Initial project

---
+++ programmable-wallet-sdk-ios-sample/programmable-wallet-sdk-ios-sample/programmable_wallet_sdk_ios_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  programmable_wallet_sdk_ios_sampleApp.swift
+//  programmable-wallet-sdk-ios-sample
+//
+//  Created by Kevin Chiang on 2023/5/18.
+//
+
+import SwiftUI
+
+@main
+struct programmable_wallet_sdk_ios_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
